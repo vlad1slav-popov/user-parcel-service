b699abca352d3a2f71b0ca7eda32ab9a5bf974c6
vlad1slav-popov <vladpopov9696@gmail.com>
2022-01-31 22:19:16 +0400

    initial commit

---
+++ src/main/java/com/api/userparcelservice/UserParcelServiceApplication.java
@@ -0,0 +1,13 @@
+package com.api.userparcelservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserParcelServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserParcelServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/api/userparcelservice/UserParcelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.userparcelservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserParcelServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
