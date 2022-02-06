f9872d1165dc6fe4851b906141a4bb852722fa37
vlad1slav-popov <vladpopov9696@gmail.com>
2022-02-06 22:14:42 +0400

    activemq added. liquibase added.

--- src/main/java/com/api/userparcelservice/controller/AuthenticationController.java
+++ src/main/java/com/api/userparcelservice/controller/AuthenticationController.java
@@ -1,11 +1,11 @@
 package com.api.userparcelservice.controller;
 
 
-import com.api.userparcelservice.dto.MqDTO;
 import com.api.userparcelservice.domain.RegisterUserRequest;
 import com.api.userparcelservice.domain.UserLoginRequest;
+import com.api.userparcelservice.dto.MqDTO;
 import com.api.userparcelservice.entity.UserEntity;
-import com.api.userparcelservice.service.UserAuthorizationService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
@@ -14,22 +14,12 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 @Slf4j
 public class AuthenticationController {
 
 
     private final JmsTemplate jmsTemplate;
-
-//    private final AuthenticationService authenticationService;
-    private final UserAuthorizationService userAuthorizationService;
-
-    public AuthenticationController(JmsTemplate jmsTemplate,
-//                                    AuthenticationService authenticationService,
-                                    UserAuthorizationService userAuthorizationService) {
-        this.jmsTemplate = jmsTemplate;
-//        this.authenticationService = authenticationService;
-        this.userAuthorizationService = userAuthorizationService;
-    }
 
 
     @PostMapping("/login")
@@ -40,7 +30,6 @@ public class AuthenticationController {
                 .receiveAndConvert("responsequeue");
 
         UserEntity userEntity = mqDTO.getUserEntity();
-
         HttpHeaders httpHeaders = new HttpHeaders();
         httpHeaders.add("Authorization", "Bearer " + mqDTO.getToken());
 
@@ -48,33 +37,14 @@ public class AuthenticationController {
         return ResponseEntity.ok()
                 .headers(httpHeaders)
                 .body(userEntity);
-
-
-
-//        return authenticationService.getLoginResponse(requestDto);
     }
 
 
     @PostMapping("/register")
     public ResponseEntity<UserEntity> register(@RequestBody RegisterUserRequest registerUserRequest) {
         jmsTemplate.convertAndSend("requestqueue", registerUserRequest);
-
         return ResponseEntity.ok((UserEntity) jmsTemplate
                 .receiveAndConvert("responsequeue"));
-    }
-
-    @GetMapping(value = "/test/{name}", produces = "application/json")
-    public UserEntity test(@PathVariable String name) {
-
-        UserLoginRequest userLoginRequest = new UserLoginRequest();
-        userLoginRequest.setUsername(name);
-        userLoginRequest.setPassword("123456");
-
-        jmsTemplate.convertAndSend("testqueue", userLoginRequest);
-
-        return (UserEntity) jmsTemplate
-                .receiveAndConvert("respqueue");
-
     }
 
 
